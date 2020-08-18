831e1ca65a3c5e8277089e1e74f448993360dc9f
tapitarias <tapitarias@gmail.com>
2020-08-17 19:11:15 -0500

    Github

---
+++ precache-manifest.f28c678be68ff0557c5cfde91c528fdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc788552fc858f3aca15c20849b788d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "060a4fad5f217b281500",
+    "url": "/static/css/main.4489ae66.chunk.css"
+  },
+  {
+    "revision": "9813c9b8a88bb1194cf6",
+    "url": "/static/js/2.78130ed0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78130ed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060a4fad5f217b281500",
+    "url": "/static/js/main.0ab07012.chunk.js"
+  },
+  {
+    "revision": "5642b91dccdf68ac6bda",
+    "url": "/static/js/runtime-main.a031da1b.js"
+  }
+]);
